9e54a27854c2c09fbf9ec34a8d3a4c503d07909f
maxwellcox <cox2mdc@gmail.com>
2020-03-24 23:40:02 -0600

    update

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,7 +1,21 @@
 // Required by Webpack - do not touch
-require.context('../', true, /\.(html|json|txt|dat)$/i)
-require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
-require.context('../stylesheets/', true, /\.(css|scss)$/i)
+//require.context('../', true, /\.(html|json|txt|dat)$/i)
+// require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
+// require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+//import "bootstrap";
+function displayMessage(){
+    let name = document.getElementById("nameInput").value
+    console.log(name)
+    alert("Welcome " + name + "!")
+}
+
+document.getElementById("submit").addEventListener('click', function(){
+    displayMessage()
+})
+
+document.getElementById("reset").addEventListener('click', function(){
+    document.getElementById("reset").clear
+})
